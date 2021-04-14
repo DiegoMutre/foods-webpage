661cf70e21ff8242d0fb7753d0b811b3f08be839
DiegoMutre <tysonmutre10@gmail.com>
2021-04-14 18:36:21 -0500

    Create change icon function

--- js/app.js
+++ js/app.js
@@ -8,4 +8,23 @@ function toggleMenu() {
     });
 }
 
+function changeIcon() {
+    let counter = 1;
+    const icons = document.querySelectorAll(".section-1-icons i");
+
+    setInterval(() => {
+        counter++;
+        console.log(icons.length);
+        const activeIcon = document.querySelector(".section-1-icons i.change");
+        activeIcon.classList.remove("change");
+        if (counter > icons.length) {
+            icons[0].classList.add("change");
+            counter = 1;
+            return;
+        }
+        activeIcon.nextElementSibling.classList.add("change");
+    }, 1000);
+}
+
 toggleMenu();
+changeIcon();
